38050558559c8c157c73737f188a3632373ed4b3
shendude <shendude@hotmail.com>
2017-03-16 02:22:14 -0700

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: 'babel-loader',
+        options: { presets: ['react', 'es2015']}
+      }]
+    }]
+  }
+};
